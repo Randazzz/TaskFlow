a6c748fe586964a179629e61400324b0cea4b01f
Randazzz <kir.liaxiwsky2015@yandex.ru>
2025-01-18 20:04:21 +0300

    Добавлена валидация пароля

--- app/api/schemas/user.py
+++ app/api/schemas/user.py
@@ -1,4 +1,6 @@
-from pydantic import BaseModel, EmailStr
+import re
+
+from pydantic import BaseModel, EmailStr, field_validator
 
 
 class UserResponse(BaseModel):
@@ -15,3 +17,17 @@ class UserCreate(BaseModel):
     email: EmailStr
     age: int
     password: str
+
+    @field_validator("password")
+    def validate_password(cls, password):
+        if len(password) < 8:
+            raise ValueError("Password must be at least 8 characters long")
+        if not re.search(r"[A-Z]", password):
+            raise ValueError("Password must contain at least one uppercase letter")
+        if not re.search(r"[a-z]", password):
+            raise ValueError("Password must contain at least one lowercase letter")
+        if not re.search(r"[0-9]", password):
+            raise ValueError("Password must contain at least one digit")
+        if not re.search(r"[\W_]", password):
+            raise ValueError("Password must contain at least one special character")
+        return password
